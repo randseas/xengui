4a02925a526d431a94d7111507579130ef537b3d
randseas <alchcraft24@gmail.com>
2026-07-06 13:04:52 +0300

    feat(vercel): update script to set up Rust and WASM with improved environment variables and paths

--- vercel.sh
+++ vercel.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Install Rustup and WASM target
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
+export CARGO_HOME="/tmp/.cargo"
+export RUSTUP_HOME="/tmp/.rustup"
+export PATH="$CARGO_HOME/bin:$PATH"
+
+mkdir -p "$CARGO_HOME/bin"
+mkdir -p "$RUSTUP_HOME"
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+
+source "$CARGO_HOME/env"
 rustup target add wasm32-unknown-unknown
 
-# Install Trunk
-curl -L --proto '=https' --tlsv1.2 -sSf https://github.com/trunk-rs/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf- -C $HOME/.cargo/bin
+curl -L --proto '=https' --tlsv1.2 -sSf https://github.com/trunk-rs/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf- -C "$CARGO_HOME/bin"
 
-# Execute compilation
-trunk build examples/basic_app/index.html --release
+trunk build --config examples/basic_app/Trunk.toml --release
