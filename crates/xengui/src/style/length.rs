d04128e611c1e08d8f749a4ce594d9618163fe3a
randseas <alchcraft24@gmail.com>
2026-07-06 20:07:51 +0300

    feat: add Length struct for handling dimensions in pixels

---
+++ crates/xengui/src/style/length.rs
@@ -0,0 +1,74 @@
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd, Default)]
+pub struct Length(f32);
+
+impl Length {
+    /// Creates the length in pixels.
+    pub const fn pixels(px: f32) -> Self {
+        Self(px)
+    }
+
+    /// Returns the value in pixels.
+    pub const fn px(&self) -> f32 {
+        self.0
+    }
+}
+
+impl From<f32> for Length {
+    fn from(value: f32) -> Self {
+        Self(value)
+    }
+}
+
+impl From<f64> for Length {
+    fn from(value: f64) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<u8> for Length {
+    fn from(value: u8) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<u16> for Length {
+    fn from(value: u16) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<u32> for Length {
+    fn from(value: u32) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<usize> for Length {
+    fn from(value: usize) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<i8> for Length {
+    fn from(value: i8) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<i16> for Length {
+    fn from(value: i16) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<i32> for Length {
+    fn from(value: i32) -> Self {
+        Self(value as f32)
+    }
+}
+
+impl From<isize> for Length {
+    fn from(value: isize) -> Self {
+        Self(value as f32)
+    }
+}
--- crates/xengui/src/style/mod.rs
+++ crates/xengui/src/style/mod.rs
@@ -1,6 +1,8 @@
-pub mod style;
 pub mod color;
 pub mod colors;
+pub mod length;
+pub mod style;
 
 pub use color::Color;
+pub use length::Length;
 pub use style::Style;
