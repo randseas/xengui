3212e62c8e121cf7ede212b86792379c55e5b1f1
randseas <alchcraft24@gmail.com>
2026-07-17 13:52:13 +0300

    fix: update default link color to BLUE_600

--- crates/xengui/src/style/properties.rs
+++ crates/xengui/src/style/properties.rs
@@ -32,7 +32,7 @@ use super::{
 
 pub const DEFAULT_FONT_SIZE: Length = Length::px(14.0);
 pub const DEFAULT_CURSOR_ICON: CursorIcon = CursorIcon::Pointer;
-pub const DEFAULT_LINK_COLOR: Color = Color::BLUE_500;
+pub const DEFAULT_LINK_COLOR: Color = Color::BLUE_600;
 
 #[derive(Default, Clone, Debug, PartialEq)]
 pub enum StyleValue<T> {
